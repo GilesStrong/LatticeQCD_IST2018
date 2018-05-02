8bb750f49ec8a9f79533b800b9ee7e17f35e1d68
GilesStrong <giles.strong@outlook.com>
2018-05-02 11:02:28 +0100

    Test

---
+++ testreader.hh
@@ -0,0 +1,14 @@
+#ifndef MAIN_HH_
+#define MAIN_HH_
+//C++
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+#include <map>
+#include <bitset>
+#endif /* MAIN_HH_ */
