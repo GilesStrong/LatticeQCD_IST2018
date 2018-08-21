3ba9ecdce558893292266f98bd8a6ee81501ba7f
GilesStrong <giles.strong@outlook.com>
2018-08-21 16:48:02 +0100

    Altering precision

--- src/main.cc
+++ src/main.cc
@@ -78,9 +78,7 @@ void runWilsonExperimentMP(Lattice* config, std::string name) {
             mean = config->calcOverallMeanWilsonLoopMP(R, T);
             outFile << R << "," << T << "," << mean << "\n";
             if (verbose != "") std::cout << mean << "\n";
-            break;
         }
-        break;
     }
 
     outFile.close();
